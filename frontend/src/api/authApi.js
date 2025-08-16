48f413ffc3197a950d554244978005af59da57b7
henil88 <henilrathod88@gmail.com>
2025-08-16 13:10:45 +0530

    all done

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiUrl = axios.create({
-  baseURL: "https://ai-caption-ganrator.onrender.com/api/auth",
+  baseURL: "http://localhost:3000/api/auth",
   withCredentials: true,
 });
 
