67b0e88d559fa681cd9bf234dd8940ed6868b6ca
henil88 <henilrathod88@gmail.com>
2025-08-16 12:52:20 +0530

    baseUrl Changed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,5 @@ const app = require("./src/app");
 const connectTODb = require("./src/db/db");
 
 connectTODb();
-console.log("PUBLIC_KEY exists?", !!process.env.IMAGEKIT_PUBLIC_KEY);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
