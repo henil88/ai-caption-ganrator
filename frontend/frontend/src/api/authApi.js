56512ec6601ae128adc8322e2428808c329da878
henil88 <henilrathod88@gmail.com>
2025-08-16 13:11:56 +0530

    remove duplicate files

--- frontend/frontend/src/api/authApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const apiUrl = axios.create({
-  baseURL: "http://localhost:3000/api/auth",
-  withCredentials: true,
-});
-
-export const registerApi = (data) => apiUrl.post("/register", data);
-export const loginApi = (data) => apiUrl.post("/login", data);
--- frontend/frontend/src/utils/formData.js
+++
@@ -1,8 +0,0 @@
-// formData.js
-export const createImageFormData = (file, lang, tone) => {
-  const formData = new FormData();
-  formData.append("image", file); // must match multer field name
-  formData.append("lang", lang);
-  formData.append("tone", tone);
-  return formData;
-};
